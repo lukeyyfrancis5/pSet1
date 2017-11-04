43411348b23a6c5841fabe7ab2095a099dff89ab
Luke Francis <lukebrandonfrancis@gmail.com>
2017-11-04 16:54:32 +0000

    finished greedy

--- greedy/greedy.c
+++ greedy/greedy.c
@@ -5,18 +5,16 @@
 int main(void) {
 
     printf("O hai! How much change is owed? ");
-}
 
-float n;
-
+    double n;
 do
 {
-    n = get_float(); // takes in a float
+    n = get_double();// takes in a float
 }
 
-while (n <= 0 );  // If n less than or = to 0 than stop
+while (n <= 0);  // If n less than or = to 0 than stop
 
-    n = int round(n *100);
+    n = round(n *100);
 
 //DOES MODULO FOR EACH COIN
 int i = 0;
@@ -45,4 +43,5 @@ while (n >= 1) //finds pennies
     n = n - 5;
 }
 
-printf("%d/n", i);
+printf("%d\n", i);
+}
